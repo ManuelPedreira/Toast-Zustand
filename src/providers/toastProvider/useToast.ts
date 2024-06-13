49d937963c683fb7ad1774c067a0029087ef8670
Manuel Pedreira <mapedgi@gmail.com>
2024-06-13 12:04:13 +0200

    changes on store functions

--- src/providers/toastProvider/useToast.ts
+++ src/providers/toastProvider/useToast.ts
@@ -9,22 +9,25 @@ type ToastStoreType = {
 const useToasts = create<ToastStoreType>(
   (set): ToastStoreType => ({
     toasts: [],
-    createToast: (newToast) => set((state) => ({ toasts: addToast(newToast, state) })),
-    deleteToast: (toastId) => set((state) => ({ toasts: removeToast(toastId, state) })),
+    createToast: (newToast) => set((state) => addToast(newToast, state)),
+    deleteToast: (toastId) => set((state) => removeToast(toastId, state)),
   })
 );
 
-const addToast = (newToast: Omit<ToastData, "id">, state: ToastStoreType): ToastData[] => {
+const addToast = (
+  newToast: Omit<ToastData, "id">,
+  state: ToastStoreType
+): Partial<ToastStoreType> => {
   const newFullToast: ToastData = { ...newToast, id: Math.random() };
 
   if (newFullToast.timeOut !== undefined)
     setTimeout(() => state.deleteToast(newFullToast.id), newFullToast.timeOut);
 
-  return [newFullToast, ...state.toasts];
+  return { toasts: [newFullToast, ...state.toasts] };
 };
 
-const removeToast = (toastId: number, state: ToastStoreType): ToastData[] => {
-  return state.toasts.filter((toast) => toast.id !== toastId);
+const removeToast = (toastId: number, state: ToastStoreType): Partial<ToastStoreType> => {
+  return { toasts: state.toasts.filter((toast) => toast.id !== toastId) };
 };
 
 export default useToasts;
